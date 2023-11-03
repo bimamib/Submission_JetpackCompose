7f353d6e43cd41791d7adb292fa868b6cf674077
bimamib <bimaprast1601@gmail.com>
2023-11-03 22:38:52 +0700

    mengisi data AnimeItem, AnimesData

---
+++ app/src/main/java/com/bima/jetanimeapp/model/AnimeItem.kt
@@ -0,0 +1,6 @@
+package com.bima.jetanimeapp.model
+
+data class AnimeItem(
+    val item: Anime,
+    val count: Int
+)
---
+++ app/src/main/java/com/bima/jetanimeapp/model/AnimesData.kt
@@ -0,0 +1,147 @@
+package com.bima.jetanimeapp.model
+
+object AnimesData {
+    val anime = listOf(
+        Anime(
+            "1",
+            "Jujutsu Kaisen",
+            "Jujutsu Kaisen (Jepang: 呪術廻戦, terj. har. \"Pertarungan Sihir\") adalah sebuah seri manga shōnen asal Jepang yang ditulis dan diilustrasikan oleh Gege Akutami. Manga ini dimuat berseri dalam majalah Weekly Shōnen Jump terbitan Shueisha sejak Maret 2018, dan telah diterbitkan menjadi dua puluh empat volume tankōbon per Oktober 2023." +
+                    "\n\nViz Media menerbitkan tiga bab pertamanya untuk program \"Jump Start\" mereka. Shueisha mulai merilis Jujutsu Kaisen dalam bahasa Inggris secara serentak dengan perilisannya di Jepang melalui platform Manga Plus sejak Januari 2019." +
+                    "\n\nManga ini telah diadaptasi menjadi seri anime yang diproduksi oleh MAPPA, dan ditayangkan sejak bulan Oktober 2020.",
+            "2020",
+            "Gege Akutami",
+            "https://static.wikia.nocookie.net/jujutsu-kaisen/images/8/88/Anime_Key_Visual_2.png/revision/latest?cb=20201212034001",
+            "https://awsimages.detik.net.id/community/media/visual/2022/12/06/komikus-manga-jujutsu-kaisen-gege-akutami_43.webp?w=1200"
+        ),
+
+        Anime(
+            "2",
+            "Spy x Family",
+            "Spy × Family (Jepang: スパイファミリー) adalah sebuah seri manga web shōnen Jepang yang ditulis dan diilustrasikan oleh Tatsuya Endō (遠藤達哉). Manga ini mulai dimuat pada platform Shōnen Jump+ milik Shueisha sejak tanggal 25 Maret 2019, dan telah dibundel menjadi dua belas volume tankōbon." +
+                    "\n\nShueisha mulai merilis seri ini dalam bahasa Inggris secara serentak dengan perilisannya di Jepang melalui platform Manga Plus sejak bulan Maret 2019. Adaptasi serial televisi anime oleh Wit Studio dan CloverWorks ditayangkan perdana pada April 2022 di Netflix.",
+            "2022",
+            "Tatsuya Endō",
+            "https://cdn0-production-images-kly.akamaized.net/wQd3McgbupEseI9b6XQAjiFfBF8=/800x1066/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3998604/original/077948100_1650275092-MV5BZmY2MDYxMjAtNDFmNS00YmY5LWJhMTItMDZkZGNkMjg5ODJiXkEyXkFqcGdeQXVyODMyNTM0MjM_._V1_.jpg",
+            "https://cdn1.booknode.com/author_picture/5065/tatsuya-endo-5065251-330-540.jpg"
+        ),
+
+        Anime(
+            "3",
+            "Shingeki no Kyujin",
+            "Shingeki no Kyojin (bahasa Jepang: 進撃の巨人, terj. har. \"Raksasa [yang] Menyerang\"), yang diterbitkan di Indonesia dengan judul Attack on Titan, adalah sebuah seri manga shōnen asal Jepang yang ditulis dan diilustrasikan oleh Hajime Isayama. Ceritanya berlatar di dunia tempat umat manusia hidup di wilayah yang dikelilingi tiga lapis tembok besar, yang melindungi mereka dari makhluk pemakan manusia berukuran raksasa yang dikenal sebagai Titan. Manga ini dimuat berseri dalam majalah Bessatsu Shōnen Magazine terbitan Kodansha sejak bulan September 2009 hingga April 2021, dan telah diterbitkan menjadi 34 volume tankōbon per bulan Desember 2020." +
+                    "\n\nManga ini telah diadaptasi menjadi sebuah seri anime yang diproduksi oleh Wit Studio (musim 1–3) dan MAPPA (musim 4). Musim pertama yang terdiri dari 25 episode ditayangkan sejak bulan April hingga September 2013, diikuti oleh musim kedua sebanyak 12 episode dari bulan April hingga Juni 2017. Musim ketiga yang terdiri dari 22 episode ditayangkan menjadi dua bagian. 12 episode pertama ditayangkan sejak bulan Juli hingga Oktober 2018 dan 10 episode tersisa ditayangkan sejak bulan April hingga Juli 2019. Musim keempat sekaligus terakhir ditayangkan sejak bulan Desember 2020.",
+            "2013",
+            "Hajime Isayama",
+            "https://cdn0-production-images-kly.akamaized.net/b2t4EeSL1f-hMFrQ91DkHXJ4eyA=/800x1066/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3373455/original/040252200_1612945370-Attack-On-Titan-Season-4-Anime-Key-Visual-Shingeki-no-Kyojin-Final-Season.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/e/e9/FIBD2023HajimeIsayama_01.jpg"
+        ),
+
+        Anime(
+            "4",
+            "Naruto",
+            "Naruto (ナルト) adalah sebuah serial manga karya Masashi Kishimoto yang diadaptasi menjadi serial anime. Manga Naruto bercerita seputar kehidupan tokoh utamanya, Naruto Uzumaki, seorang ninja yang hiperaktif, periang, dan ambisius yang ingin mewujudkan keinginannya untuk mendapatkan gelar Hokage, pemimpin dan ninja terkuat di desanya. Serial ini didasarkan pada komik one-shot oleh Kishimoto yang diterbitkan dalam edisi Akamaru Jump pada Agustus 1997." +
+                    "\n\nManga Naruto pertama kali diterbitkan di Jepang oleh Shueisha pada tahun 1999 dalam edisi ke-43 majalah Shonen Jump. Di Indonesia, manga ini diterbitkan oleh Elex Media Komputindo. Popularitas dan panjang seri Naruto sendiri (terutama di Jepang) menyaingi Dragon Ball karya Akira Toriyama, sedangkan serial anime Naruto, diproduksi oleh Studio Pierrot dan Aniplex, disiarkan secara perdana di Jepang oleh jaringan TV Tokyo dan juga oleh jaringan televisi satelit khusus anime, seperti Animax dan stasiun televisi lainnya, pada 3 Oktober 2002 sampai sekarang." +
+                    "\n\nViz Media memiliki lisensi manga dan anime produksi Amerika Utara. Viz telah menerbitkan seri ini ke dalam majalah mereka, yaitu Shonen Jump, serta beberapa volume. Seri anime ini mulai ditayangkan di Amerika Serikat dan Kanada pada tahun 2005, dan kemudian di Inggris dan Australia pada tahun 2006 dan 2007.",
+            "2002",
+            "Masashi Kishimoto",
+            "https://assets.cdn.moviepilot.de/files/289cc208d3ac718759363f8c781d607cd5454300928b4746c5541d74ec20/copyright/naruto-poster-01.jpg",
+            "https://static.wikia.nocookie.net/fictional-battle-omniverse/images/6/62/Masashi_Kishimoto.png/revision/latest?cb=20151227013410"
+        ),
+
+        Anime(
+            "5",
+            "Demon Slayer: Kimetsu no Yaiba",
+            "Kimetsu no Yaiba (Jepang: 鬼滅の刃, terj. har. \"Korps Pembasmi Iblis\"[4]), yang diterbitkan di Indonesia dengan judul Demon Slayer: Kimetsu no Yaiba, adalah sebuah seri manga Jepang yang ditulis dan diilustrasikan oleh Koyoharu Gotōge. Ceritanya mengisahkan tentang Tanjiro Kamado, seorang anak laki-laki yang menjadi pembasmi iblis setelah keluarganya dibantai oleh iblis dan adik perempuannya yang bernama Nezuko diubah menjadi iblis." +
+                    "\n\nManga ini dimuat berseri dalam majalah Weekly Shōnen Jump terbitan Shueisha sejak bulan Februari 2016 hingga Mei 2020, dan telah dibundel menjadi dua puluh satu volume tankōbon hingga bulan Juli 2020. Manga ini diterbitkan dalam bahasa Inggris oleh Viz Media, sementara Shueisha merilisnya dalam bahasa Inggris dan Spanyol secara serentak dengan perilisannya di Jepang melalui platform Manga Plus milik mereka." +
+                    "\n\nManga ini diadaptasi menjadi sebuah seri anime yang diproduksi oleh Ufotable dan ditayangkan sejak bulan April hingga September 2019. Sekuel berupa film layar lebar diumumkan ketika episode terakhir disiarkan dan dijadwalkan untuk dirilis pada bulan Oktober 2020.",
+            "2019",
+            "Koyoharu Gotōge",
+            "https://scontent.fcgk42-1.fna.fbcdn.net/v/t39.30808-6/240932807_4042044779240258_4108958082190960483_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Dh87sLAM2RYAX__WufN&_nc_ht=scontent.fcgk42-1.fna&oh=00_AfDcatlsehgyuwcJD8iyc9snVhEmG3bmCL1OV3kF8WlhMg&oe=654A86DC",
+            "https://m.media-amazon.com/images/M/MV5BYTIxNjk3YjItYmYzMC00ZTdmLTk0NGUtZmNlZTA0NWFkZDMwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg",
+        ),
+
+        Anime(
+            "6",
+            "Detektif Conan",
+            "Detektif Conan (名探偵コナン, Meitantei Konan), yang juga dikenal sebagai Case Closed dan Detective Conan, adalah seri manga detektif Jepang yang ditulis dan diilustrasikan oleh Gosho Aoyama. Serial ini diserialisasikan dalam majalah manga shōnen Weekly Shōnen Sunday yang diterbitkan oleh Shogakukan sejak Januari 1994, dengan bab-babnya dikumpulkan dalam beberapa volume tankōbon." +
+                    "\n\nKarena masalah hukum dengan nama Detective Conan, rilisan bahasa Inggris dari Funimation dan Viz diubah namanya menjadi Case Closed. Serial ini bercerita mengenai detektif SMA bernama Shinichi Kudo (atau Jimmy Kudo dalam beberapa terjemahan bahasa Inggris) yang tubuhnya menyusut menjadi kecil ketika menyelidiki sebuah organisasi misterius dan umumnya memecahkan banyak kasus dengan meniru suara dari ayah teman masa kecilnya dan berbagai karakter lain." +
+                    "\n\nManga ini diadaptasi menjadi serial televisi anime oleh Yomiuri Telecasting Corporation dan TMS Entertainment, yang ditayangkan perdana pada Januari 1996. Anime ini menghasilkan film animasi, animasi video orisinal, permainan video, cakram audio dan episode laga hidup.",
+            "1996",
+            "Gosho Aoyama",
+            "https://m.media-amazon.com/images/M/MV5BNTE5NzNhNzYtY2YxOC00ZjY2LTk5MzctNWY0NDdiN2IxNzc0XkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_.jpg",
+            "https://www.whiteboardjournal.com/wp-content/uploads/2016/12/goshoaoyama04b.jpg"
+        ),
+
+        Anime(
+            "7",
+            "Boruto: Naruto Next Generations",
+            "Boruto: Naruto Next Generations[a] adalah sebuah seri manga asal Jepang yang ditulis oleh Ukyo Kodachi dan Masashi Kishimoto dan diilustrasikan oleh Mikio Ikemoto. Manga ini dimuat berseri dalam majalah Weekly Shōnen Jump terbitan Shueisha pada Mei 2016, sebelum dipindahkan ke majalah lain milik Shueisha bertajuk V Jump pada Juli 2019. Pada November 2020 Kodachi mengundurkan diri, dan digantikan oleh Kishimoto. Boruto juga merupakan cerita spin-off dan sekuel dari manga Naruto karya Masashi Kishimoto, yang mengisahkan kehidupan putra Naruto Uzumaki, Boruto Uzumaki, dan teman-temannya." +
+                    "\n\nBoruto: Naruto Next Generations adalah sebuah seri manga asal Jepang yang ditulis oleh Ukyo Kodachi dan Masashi Kishimoto dan diilustrasikan oleh Mikio Ikemoto. Manga ini dimuat berseri dalam majalah Weekly Shōnen Jump terbitan Shueisha pada Mei 2016, sebelum dipindahkan ke majalah lain milik Shueisha bertajuk V Jump pada Juli 2019. Pada November 2020 Kodachi mengundurkan diri, dan digantikan oleh Kishimoto. Boruto juga merupakan cerita spin-off dan sekuel dari manga Naruto karya Masashi Kishimoto, yang mengisahkan kehidupan putra Naruto Uzumaki, Boruto Uzumaki, dan teman-temannya." +
+                    "\n\nMeski Ukyō Kodachi dan Ikemoto bekerja sama untuk versi manga, Ukyō Kodachi juga mengawasi adaptasi anime Boruto bersama dengan Masashi Kishimoto. Sebuah adaptasi anime yang disutradarai oleh Noriyuki Abe mulai tayang di TV Tokyo sejak tanggal 5 April 2017.",
+            "2017",
+            "Masashi Kishimoto",
+            "https://upload.wikimedia.org/wikipedia/id/thumb/d/db/Boruto_manga_vol_1.jpg/220px-Boruto_manga_vol_1.jpg",
+            "https://static.wikia.nocookie.net/samurai8/images/2/2f/Kishimoto_With_Storyboards_of_Samurai_8.png/revision/latest?cb=20190503013136"
+        ),
+
+        Anime(
+            "8",
+            "One Piece",
+            "One Piece (Jepang: ワンピース, Hepburn: Wan Pīsu) adalah sebuah seri manga Jepang yang ditulis dan diilustrasikan oleh Eiichiro Oda. Manga ini telah dimuat di majalah Weekly Shōnen Jump milik Shueisha sejak tanggal 22 Juli 1997, dan telah dibundel menjadi 105 volume tankōbon hingga Maret 2023. Ceritanya mengisahkan petualangan Monkey D. Luffy, seorang anak laki-laki yang memiliki kemampuan tubuh elastis seperti karet setelah memakan Buah Iblis secara tidak disengaja." +
+                    "\n\nLuffy bersama kru bajak lautnya, yang dinamakan Bajak Laut Topi Jerami, menjelajahi Grand Line untuk mencari harta karun terbesar di dunia yang dikenal sebagai \"One Piece\" dalam rangka untuk menjadi Raja Bajak Laut yang berikutnya." +
+                    "\n\nManga ini telah diadaptasi menjadi sebuah animasi video asli (OVA) yang diproduksi oleh Production I.G pada tahun 1998, dan sebuah serial anime yang diproduksi oleh Toei Animation, yang mulai tayang di Jepang pada tahun 1999. Sebagai tambahan, Toei juga telah memproduksi tiga belas film animasi, satu OVA dan sebelas episode khusus televisi.",
+            "1999",
+            "Eiichiro Oda",
+            "https://m.media-amazon.com/images/M/MV5BODcwNWE3OTMtMDc3MS00NDFjLWE1OTAtNDU3NjgxODMxY2UyXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg",
+            "https://awsimages.detik.net.id/community/media/visual/2018/01/16/49212982-36e7-4173-80a5-27eed01dc181_34.jpeg?w=600&q=90"
+        ),
+
+        Anime(
+            "9",
+            "Black Clover",
+            "Black Clover (Jepang: ブラッククローバー, Hepburn: Burakku Kurōbā) adalah sebuah seri manga shōnen bergenre fantasi asal Jepang yang ditulis dan diilustrasikan oleh Yūki Tabata. Ceritanya mengisahkan tentang seorang anak laki-laki bernama Asta yang lahir tanpa kekuatan sihir, suatu fenomena yang tidak normal di dunia tempatnya tinggal." +
+                    "\n\nBersama dengan teman-temannya dari Banteng hitam, dia bercita-cita untuk menjadi Kaisar sihir. Manga ini dimuat berseri dalam majalah Weekly Shōnen Jump terbitan Shueisha sejak bulan Februari 2015, dan telah dibundel menjadi 35 volume tankōbon per bulan Juni 2023." +
+                    "\n\nManga ini pertama kali diadaptasi menjadi sebuah video animasi orisinal (OVA) yang diproduksi oleh Xebec Zwei dan dirilis pada tahun 2017, kemudian diadaptasi menjadi sebuah seri anime yang diproduksi oleh Pierrot, yang ditayangkan di Jepang sejak bulan Oktober 2017.",
+            "2017",
+            "Yūki Tabata",
+            "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/e108ae17d8d0407417cac40eb52d849a.jpe",
+            "https://cdn.myanimelist.net/images/voiceactors/1/58942.jpg"
+        ),
+
+        Anime(
+            "10",
+            "My Hero Academia",
+            "Boku no Hero Academia (Jepang: 僕のヒーローアカデミア, Hepburn: Boku no Hīrō Akademia), yang diterbitkan di Indonesia dengan judul My Hero Academia, adalah sebuah seri manga shōnen Jepang bertema pahlawan super yang ditulis dan diilustrasikan oleh Kōhei Horikoshi. Manga ini mulai dimuat dalam majalah Weekly Shōnen Jump sejak bulan Juli 2014, dan telah dibundel menjadi 37 volume tankōbon hingga bulan Februari 2023." +
+                    "\n\nCeritanya mengisahkan tentang Izuku Midoriya (nama pahlawan: Deku), seorang anak lelaki tanpa kekuatan super (yang disebut quirk) di dunia tempat hal seperti itu sudah menjadi sesuatu yang umum, tetapi masih bercita-cita untuk menjadi seorang pahlawan. Ia kemudian bertemu dengan pahlawan terhebat di Jepang, All Might, yang memberikan quirk miliknya kepada Izuku setelah melihat potensinya, dan kemudian memasukkannya dalam sebuah SMA prestisius yang dikhususkan untuk para pahlawan muda yang sedang dalam pelatihan." +
+                    "\n\nManga ini telah diadaptasi menjadi sebuah anime yang diproduksi oleh Bones. Musim pertamanya ditayangkan di Jepang sejak tanggal 3 April hingga 26 Juni 2016, diikuti oleh musim kedua yang ditayangkan sejak tanggal 1 April hingga 30 September 2017, dilanjutkan dengan musim ketiga sejak tanggal 7 April hingga 29 September 2018, dan musim keempat sejak tanggal 12 Oktober 2019 hingga 4 April 2020.",
+            "2016",
+            "Kōhei Horikoshi",
+            "https://m.media-amazon.com/images/M/MV5BOGZmYjdjN2UtNjAwZi00YmEyLWFhNTEtNjM1OTc5ODg0MGEyXkEyXkFqcGdeQXVyMTA1NjQyNjkw._V1_.jpg",
+            "https://cdn.fandomwire.com/wp-content/uploads/2023/08/19113519/Kohei-Horikoshi.jpg"
+        ),
+
+        Anime(
+            "11",
+            "One-Punch Man",
+            "One Punch Man ( ワンパンマン Pria Satu Pukulan) adalah sebuah serial manga Jepang yang menceritakan seorang superhero yang bernama Saitama. One-Punch Man dibuat oleh seorang penulis yang asal jepang dengan nama samaran ONE yang mulai diterbitkan pada awal tahun 2009. Serial ini menjadi viral sehingga mampu melampaui 7,9 juta penggemar pada bulan Juni 2012. One-Punch Man merupakan kontraksi wanpanchi (\"One-Punch\") yang artinya satu pukulan." +
+                    "\n\nSebuah remake manga digital dari seri ini, yang diilustrasikan oleh Yusuke Murata, mulai dipublikasikan di situs Web Komedi Langsung Jumpa Shueisha pada tahun 2012. Bab-bab tersebut dikumpulkan secara berkala dan dicetak ke dalam volume tankōbon, dengan dua puluh delapan dirilis pada tanggal 2 Juni 2023." +
+                    "\n\nAdaptasi televisi anime oleh Madhouse ditayangkan di Jepang antara bulan Oktober dan Desember 2015. Itu dijuluki dalam bahasa Inggris selama musim panas 2016, dan kemudian pada tahun itu sebuah musim kedua yang direncanakan diumumkan.",
+            "2015",
+            "Yusuke Murata",
+            "https://m.media-amazon.com/images/M/MV5BZjJlNzE5YzEtYzQwYS00NTBjLTk5YzAtYzUwOWQyM2E3OGI2XkEyXkFqcGdeQXVyNTgyNTA4MjM@._V1_.jpg",
+            "https://api.duniagames.co.id/api/content/upload/file/19946754191675935327.jpg"
+        ),
+
+        Anime(
+            "12",
+            "Tokyo Ghoul",
+            "Tokyo Ghoul (Jepang: 東京喰種トーキョーグール, Hepburn: Tōkyō Gūru) adalah sebuah seri manga seinen Jepang bergenre fantasi gelap yang ditulis dan diilustrasikan oleh Sui Ishida. Manga ini mulai dimuat dalam majalah Weekly Young Jump terbitan Shueisha sejak bulan September 2011 hingga September 2014, dan telah dibundel menjadi empat belas volume tankōbon hingga bulan Agustus 2014." +
+                    "\n\nSebuah sekuel yang berjudul Tokyo Ghoul:re mulai dimuat di majalah yang sama antara bulan Oktober 2014 hingga Juli 2018, dan kemudian dibundel menjadi enam belas volume tankōbon. Sebuah cerita prekuel bertajuk Tokyo Ghoul [Jack] dirilis secara daring di Jump Live." +
+                    "\n\nSebuah adaptasi anime sebanyak 12 episode yang diproduksi oleh studio Pierrot ditayangkan di saluran Tokyo MX sejak tanggal 4 Juli 2014 hingga 19 September 2014.",
+            "2014",
+            "Sui Ishida",
+            "https://static.posters.cz/image/750/plakaty/tokyo-ghoul-ken-kaneki-i28409.jpg",
+            "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ2tiGSdpLMhlmD4MCZZqd7KKJMYZPPyz7UDSAYrO0C3f5smXbN"
+        )
+    )
+}
