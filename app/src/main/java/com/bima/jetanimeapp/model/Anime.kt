4c361038661b36b2de4763ea43ce33321fca8325
bimamib <bimaprast1601@gmail.com>
2023-11-03 20:21:34 +0700

    membuat data class Anime

---
+++ app/src/main/java/com/bima/jetanimeapp/model/Anime.kt
@@ -0,0 +1,11 @@
+package com.bima.jetanimeapp.model
+
+data class Anime(
+    val id: String,
+    val animeName: String,
+    val desc: String,
+    val year: String,
+    val author: String,
+    val bannerUrl: String,
+    val photoAuthorUrl: String
+)
